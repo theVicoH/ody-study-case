04ba2490411302786095fc19d4cf1b8b030b8e8f
theVicoH <huang.victor.mail@gmail.com>
2026-05-09 19:53:49 +0200

    feat: update client import

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -1,1 +1,12 @@
-export {};
+export { useRestaurants } from "@/hooks/use-restaurants/use-restaurants.hook";
+export { useRestaurantSelectionStore } from "@/stores/restaurant-selection/restaurant-selection.store";
+export type {
+  Restaurant,
+  RestaurantCustomer,
+  RestaurantDetailedStats,
+  RestaurantMenuItem,
+  RestaurantOrder,
+  RestaurantPerformance,
+  RestaurantSettings,
+  RestaurantStats
+} from "@/types/restaurant/restaurant.types";
