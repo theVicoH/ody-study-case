65cbf1ef7bd1dd2fa960ca6021b10ea0326eb0e6
theVicoH <huang.victor.mail@gmail.com>
2026-05-09 19:47:56 +0200

    feat: setup avatar shadcn

---
+++ packages/ui/src/components/ui/avatar.stories.tsx
@@ -0,0 +1,37 @@
+import { Avatar, AvatarFallback, AvatarImage } from "./avatar";
+
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta: Meta<typeof Avatar> = {
+  title: "UI/Avatar",
+  component: Avatar
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Avatar>;
+
+export const Default: Story = {
+  render: () => (
+    <Avatar>
+      <AvatarImage src="" alt="User" />
+      <AvatarFallback>VC</AvatarFallback>
+    </Avatar>
+  )
+};
+
+export const Small: Story = {
+  render: () => (
+    <Avatar size="sm">
+      <AvatarFallback>SM</AvatarFallback>
+    </Avatar>
+  )
+};
+
+export const Large: Story = {
+  render: () => (
+    <Avatar size="lg">
+      <AvatarFallback>LG</AvatarFallback>
+    </Avatar>
+  )
+};
